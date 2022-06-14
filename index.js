661db68b0d24fe5260babeec2a5b8bb71888f00e
NazmusSakib-tech <sakibkhancse09@gmail.com>
2022-06-14 13:30:57 +0600

    passport error solve

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
 const initializePassport = require("./passport-config");
-initializePassport(passport, (email) =>
-  users.find((user) => user.email === email)
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
 );
 
 const users = [];
@@ -32,10 +34,15 @@ app.use(passport.session());
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+app.get("/", checkAuthenticated, (req, res) => {
   res.render("index.ejs", { name: "people" });
 });
-app.get(
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post(
   "/login",
   passport.authenticate("local", {
     successRedirect: "/",
@@ -52,7 +59,7 @@ app.post("/register", async (req, res) => {
     const hashPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
       id: Date.now().toString(),
-      name: req.body.name,
+      name: req.body.username,
       email: req.body.email,
       password: hashPassword,
     });
@@ -63,5 +70,12 @@ app.post("/register", async (req, res) => {
   }
   console.log(users);
 });
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 app.listen(8000, () => console.log("Server is running on port 8000"));
--- passport-config.js
+++ passport-config.js
@@ -1,6 +1,6 @@
-const localStrategy = require("passport-local").Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
-function initializePassport(passport, getUserByEmail) {
+function initializePassport(passport, getUserByEmail, getUserById) {
   const authenticateUser = (email, password, done) => {
     const user = getUserByEmail(email);
     if (user == null) {
@@ -14,9 +14,15 @@ function initializePassport(passport, getUserByEmail) {
       }
     } catch (error) {}
   };
-  passport.use(new localStrategy({ usernameField: "email" }, authenticateUser));
+  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
   passport.serializeUser((user, done) => done(null, user.id));
-  passport.deserializeUser((id, done) => {});
+  passport.deserializeUser((id, done) => {
+    const user = getUserById(id);
+    if (user == null) {
+      return done(new Error("User not found"));
+    }
+    done(null, user);
+  });
 }
 
 module.exports = initializePassport;
